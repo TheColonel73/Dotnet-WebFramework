5f166b66e6151ce536ae50bd9a68b0a80f7c9912
Gary Hodgson <garyhodgson73@gmail.com>
2022-11-28 01:03:45 +0000

    first commit

---
+++ src/BasicWebApplication/BasicWebApplication/Gruntfile.js
@@ -0,0 +1,30 @@
+﻿/// <binding AfterBuild='copy' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: ["wwwroot/libs/*", "temp/"],
+        copy: {
+            js: {
+                expand: true,
+                flatten: true,
+                cwd: "./node_modules",
+                dest: "./wwwroot/libs/js/",
+                src: ["./jquery/dist/jquery.js",
+                    "./jquery-validation/dist/jquery.validate.js",
+                    "./jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js",
+                    "./bootstrap/dist/js/*",
+                    "./luxon/build/global/luxon.js"
+                ]
+            },
+            css: {
+                expand: true,
+                flatten: true,
+                cwd: "./node_modules",
+                dest: "./wwwroot/libs/css/",
+                src: ["./bootstrap/dist/css/*"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks('grunt-contrib-copy');
+};
